7e75aed0c3ed0cc36d69064d2b348a0afae499a7
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2023-11-04 19:12:40 +0530

    feat: flow UI, mobile view

--- src/components/client-form.jsx
+++ src/components/client-form.jsx
@@ -1,4 +1,4 @@
-import { ArrowLeft, ArrowLeftCircleIcon, ArrowRightCircleIcon, CopyIcon, Pause, Play } from 'lucide-react'
+import { ArrowLeft, ArrowLeftCircleIcon, ArrowRightCircleIcon, CopyIcon, Loader, Pause, Play } from 'lucide-react'
 import React, { useEffect, useRef, useState } from 'react'
 import { Button } from './ui/button'
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
@@ -7,6 +7,7 @@ import dynamic from 'next/dynamic'
 import { RadioGroup, RadioGroupItem } from './ui/radio-group'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { Textarea } from './ui/textarea'
+import { Audio } from 'react-loader-spinner'
 
 const data = [
     {
@@ -84,35 +85,49 @@ const data = [
     ]
 
 
-const UserForm = ({data, setCurrentLevel, setAnswers, isLast}) => {
+const UserForm = ({data, setCurrentLevel, setAnswers, isLast, isFirst}) => {
     const [hoverControls] = useAutoAnimate();
     const [showControls, setShowControls] = useState(true);
     const [showPlay, setShowPlay] = useState(true);
     const [answer, setAnswer] = useState();
+    const [showLoader, setShowLoader] = useState(true);
     const videoRef = useRef();
 
 
     useEffect(() => {
         const timeout = setTimeout(() => setShowControls(false), 2000)
         if(videoRef.current) {
+            setShowLoader(false)
             videoRef.current.play();
         }
-        return () => {clearTimeout(timeout); videoRef.current?.pause()}
+        return () => {clearTimeout(timeout);}
     }, []);
 
   return (
-    <Dialog defaultOpen={true} open={true}>
+    <Dialog defaultOpen={true} open={true} className="outline-none">
         <DialogContent className="p-0 w-full h-full md:max-w-[80%] md:h-[80%] flex flex-row gap-0">
-            <div ref={hoverControls} className='relative w-1/2 h-full bg-cover overflow-hidden' onMouseEnter={() => {setShowControls(true)}} onMouseLeave={() =>setTimeout(() => setShowControls(false), 1000)}>
+            <div ref={hoverControls} className='relative md:w-1/2 w-full h-full bg-cover overflow-hidden' onMouseOver={() => {setShowControls(true)}} onMouseLeave={() => setTimeout(() => setShowControls(false), 1000)}>
             {
                 showControls &&
-                <Button className="absolute z-10 rounded-full top-[45%] left-[45%] w-12 h-12 " variant="outline" size="icon" onClick={() => !!videoRef.current.paused ? videoRef.current.play():  videoRef.current.pause()}>{
-                    !!videoRef.current?.paused && !showPlay ?  <Play className="h-10 w-10" />: <Pause className="h-10 w-10" />
+                (
+                    <>
+                    {/* <Button className="absolute z-10 rounded-full top-0 right-0 w-20 h-20 p-4 " variant="outline" size="icon" onClick={() => {}}>
+
+                    </Button> */}
+                    {
+                        showLoader ? (<>
+                            <Loader />
+                        </>): <Button className="absolute z-50 rounded-full top-[45%] left-[45%] w-20 h-20 p-4 " variant="outline" size="icon" onClick={() => !!videoRef.current.paused ? videoRef.current.play():  videoRef.current.pause()}>{
+                     !showPlay ?  <Play className="h-7 w-7" />: <Pause className="h-7 w-7" />
                 }</Button>
+                    }
+                    </>
+                
+                )
             }
-                <video ref={videoRef} onPlay={() => setShowPlay(true)} onPause={() => setShowPlay(false)} className='absolute w-full h-full top-0 left-0 object-cover rounded-2' src={data.video} autoFocus autoPlay />
+                <video ref={videoRef} onPlay={() => {setShowPlay(true); setShowLoader(false)}} onPause={() => setShowPlay(false)} className='absolute w-full h-full top-0 left-0 object-cover rounded-2' src={data.video} onLoad={() => showLoader(true)} autoFocus autoPlay />
             </div>
-            <div className='w-1/2 py-8 px-5 flex flex-col justify-between'>
+            <div className='hidden md:flex w-1/2 py-8 px-5 flex-col justify-between'>
             <div>
                 <h1 className='font-bold text-xl'>{data.label}</h1>
                  {!!data.type && (data.type === "text" ? (
@@ -135,7 +150,44 @@ const UserForm = ({data, setCurrentLevel, setAnswers, isLast}) => {
                             </>
                         ) : (
                             <>
-                            <Button variant='outline' className="flex gap-1"><span><ArrowLeftCircleIcon size={15} /></span>Prev</Button>
+                            {!isFirst ? <Button variant='outline' className="flex gap-1"><span><ArrowLeftCircleIcon size={15} /></span>Prev</Button>: <div className='flex-1' />}
+                            <Button className="flex gap-1" onClick={() =>{
+                                if(!!data.nextBlock && data.type === 'text')
+                                    setCurrentLevel(data.nextBlock)
+                                    else {
+                                        const nextBlockIndex = data.options.findIndex((i) => i.value === answer);
+                                        setCurrentLevel(data.options[nextBlockIndex].nextBlock)
+                                    }
+                            }} disabled={!answer}> Next { "   "} <span><ArrowRightCircleIcon size={15} /></span></Button>
+                            </>
+                        )
+                    }
+                </div>
+            </div>
+            <div className='md:hidden w-full h-auto max-h-[50%] overflow-auto absolute z-20 bottom-0 py-8 px-5 flex flex-col justify-between bg-gradient-to-t from-black/80 via-black/50 to-black/5 opacity-100'>
+                <div className='overflow-auto text-white'>
+                    <h1 className='font-bold text-xl'>{data.label}</h1>
+                    {!!data.type && (data.type === "text" ? (
+                        <Textarea className="mt-5 text-black" placeholder="something" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+                    ): (
+                        <RadioGroup value={answer} onValueChange={setAnswer} className="mt-7">
+                            {data.options.map((option, index) => (<div className="flex items-center space-x-3 p-2 border w-full rounded-xl cursor-pointer">
+                                <RadioGroupItem value={option.value} id={`r${index+1}`} />
+                                <Label htmlFor={`r${index+1}`} className="w-full m-2">{option.value}</Label>
+                            </div>)) }
+                        </RadioGroup>
+                    ))}
+                </div>
+                <div className='flex flex-row justify-between items-center mt-2 text-white'>
+                    {
+                        isLast ? (
+                            <>
+                                <div class="flex-1" />
+                                <Button variant="">Submit 🔥</Button>
+                            </>
+                        ) : (
+                            <>
+                            {!isFirst ? <Button variant='outline' className="flex gap-1 text-black"><span><ArrowLeftCircleIcon size={15} /></span>Prev</Button> :  <div className='flex-1' />}
                             <Button className="flex gap-1" onClick={() =>{
                                 if(!!data.nextBlock && data.type === 'text')
                                     setCurrentLevel(data.nextBlock)
@@ -162,7 +214,7 @@ const UserFormLevels = () => {
         <div ref={parent}>
             {!!data && data.map((item, index, data) => {
                 if(index === currentLevel) {
-                  return  <UserForm isLast={index === (data.length-1)} data={item} setCurrentLevel={setCurrentLevel} setAnswers={setAnswers} />
+                  return  <UserForm isLast={index === (data.length-1)} isFirst={index === 0} data={item} setCurrentLevel={setCurrentLevel} setAnswers={setAnswers} />
                 }
             })}
         </div>
