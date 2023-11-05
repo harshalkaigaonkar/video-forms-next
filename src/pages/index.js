085c6d093e1d89686b80b833ffe9072d747d5637
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2023-11-05 06:17:42 +0530

    listings desgin changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -9,23 +10,18 @@ import {
 import { useToast } from "@/components/ui/use-toast";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ forms }) {
   const { toast } = useToast();
-  const handleCopy = (formId) => () => {
-    navigator.clipboard
-      .writeText(`${window.location.origin}/form/${formId}`)
-      .then(() => {
-        toast({
-          variant: "success",
-          description: "Copied to clipboard",
-        });
+  const handleCopy = () => {
+    navigator.clipboard.writeText(window.location.href).then(() => {
+      toast({
+        description: "Copied to clipboard",
       });
+    });
   };
-
   return (
     <main
       className={` text-black min-h-screen px-2 sm:px-4 h-screen w-screen ${inter.className}`}
@@ -33,43 +29,33 @@ export default function Home({ forms }) {
       <nav className="sticky bg-white justify-between flex items-center border-b gap-2 w-full p-4 px-10 top-0">
         <div className="flex items-center gap-2">
           <h3 className=" font-bold text-2xl   bg-clip-text text-transparent bg-gradient-to-b from-gray-300 via-gray-900 to-black ">
-            <Link href="/">VideoForms</Link>
+            VideoForms
           </h3>
         </div>
         <Link className=" hidden sm:block" href="/create">
           <Button>Create Form</Button>
         </Link>
       </nav>
-      {!!forms?.length ? (
-        <div className=" max-w-[1080px] px-0 py-4 sm:py-8 w-full mx-auto gap-4 grid grid-cols-1 sm:grid-cols-2 ">
-          {forms.map((item) => (
-            <Card className="px-0" key={item.id}>
-              <CardContent>
-                <video
-                  autoPlay
-                  loop
-                  muted
-                  src={item.questions[0].video}
-                ></video>
-              </CardContent>
-              <CardHeader>
-                <CardTitle>{item.name}</CardTitle>
-              </CardHeader>
-              <CardFooter className="flex justify-end">
-                <Button onClick={handleCopy(item.id)}>🚀 Share</Button>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
-      ) : (
-        <div className="mx-auto min-h-[70vh] flex flex-col items-center justify-center text-center">
-          <Image width={400} height={500} alt="Empty" src="/empty.svg" />
-          <h3 className=" text-2xl ">No forms found</h3>
-        </div>
-      )}
-      <Link href={"/create"} className="absolute bottom-4 right-4 sm:hidden">
-        <Button>Create Form</Button>
-      </Link>
+      <h3 className="mx-10 mt-8 font-medium text-md text-black/50 border w-fit px-4 py-2 rounded-xl">My Forms / Admin</h3>
+      <div className="px-10 mt-3 py-4 sm:py-3 w-full gap-3 flex flex-row flex-wrap justify-start items-center">
+        {forms.map((item) => (
+          <Card className="px-0 w-80" key={item.id}>
+            <CardContent className="relative h-96">
+              <video autoPlay loop muted src={item.questions[0].video} className="absolute w-full h-full top-0 left-0 object-cover rounded-t-md rounded-b-none"></video>
+            </CardContent>
+            <CardHeader>
+              <CardTitle>{item.name}</CardTitle>
+              <CardDescription className="text-black/30">{item.questions.length} questions</CardDescription>
+            </CardHeader>
+            <CardFooter className="flex justify-end">
+              <Button onClick={handleCopy}>🚀 Share</Button>
+            </CardFooter>
+          </Card>
+        ))}
+        <Link href={"/create"} className="absolute bottom-4 right-4 sm:hidden">
+          <Button>Create Form</Button>
+        </Link>
+      </div>
     </main>
   );
 }
