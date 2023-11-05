7ff4d05e23780580d176aba45a22c3257b9fda0e
Haridarshan Choudhary <haridarshanofficial@gmail.com>
2023-11-05 05:50:50 +0530

    chore: link the home logo

--- src/pages/create.js
+++ src/pages/create.js
@@ -3,6 +3,7 @@ import { ReactFlowProvider } from "reactflow";
 import NodesProvider from "@/providers/NodesProvider";
 import { Toaster } from "@/components/ui/toaster";
 import FlowEditor from "@/components/flow-editor";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function Create() {
         <nav className="fixed z-50 bg-white flex items-center border-b gap-2 w-full p-4 px-10 top-0">
           <div className="flex flex-wrap items-center gap-2">
             <h3 className=" font-bold text-xl  sm:text-2xl   bg-clip-text text-transparent bg-gradient-to-b from-gray-300 via-gray-900 to-black ">
-              VideoForms
+              <Link href="/">VideoForms</Link>
             </h3>
           </div>
         </nav>
